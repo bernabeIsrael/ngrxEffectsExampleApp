2ab8de2e3795f13229700ac824361888b93045ac
Israel Bernabe <israel.bernabe@gmail.com>
2020-12-25 17:46:09 -0400

    Adding components routing and basic structure

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private url = 'https://reqres.in/api';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getUsers(){
+    return this.http.get(`${this.url}/users`)
+      .pipe(
+        map(resp => {
+          return resp['data'];
+        })
+      );
+  }
+}
